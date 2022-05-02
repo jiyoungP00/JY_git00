4d72d48b89627dfc9e6bc5bb6e4a114b575af99a
jiyoungP00 <101079009+jiyoungP00@users.noreply.github.com>
2022-05-02 14:54:24 +0900

    Update test-repo.py

--- test-repo.py
+++ test-repo.py
@@ -1,4 +1,6 @@
 print("2022년 03월 11일")
 print("2022년 03월 18일 (수정)")
 print("220311 오픈소스SW 및 실습__")
-print("Park Jiyoung 202004240 컴퓨터전자시스템공학부")
+print('Park Jiyoung')
+print('202004240')
+print('컴퓨터전자시스템공학부')
